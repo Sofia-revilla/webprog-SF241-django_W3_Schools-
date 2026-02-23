1c7d489bc457b9c7f823bebf42e482694d3b3f1b
Ma. Sofia Anne Revilla <mcrevilla@student.apc.edu.ph>
2026-02-23 14:33:04 +0000

    Fixed URL routing to resolve 404 error

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
  
 def members(request):
-    return HttpResponse("Hello world Everyone, SF241, welcome to the members page of my tennis club!")
+    return HttpResponse("<h2>Revilla: Hello world Everyone, SF241, welcome to the members page of my tennis club!</h2>")
